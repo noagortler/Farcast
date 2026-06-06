65de2f4ea76b03685d342e2bf0b9179a0dca28a8
noagortler <noagortler@gmail.com>
2026-06-05 18:30:10 -0700

    remove About page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Welcome from './components/Welcome'
 import Onboarding from './components/Onboarding'
 import Home from './components/Home'
 import Settings from './components/Settings'
-import About from './components/About'
 
 function App() {
   const [page, setPage] = useState('welcome')
@@ -12,7 +11,6 @@ function App() {
   if (page === 'onboarding') return <Onboarding setPage={setPage} />
   if (page === 'home') return <Home setPage={setPage} />
   if (page === 'settings') return <Settings setPage={setPage} />
-  if (page === 'about') return <About setPage={setPage} />
 }
 
 export default App
--- src/components/About.jsx
+++
@@ -1,5 +0,0 @@
-function About() {
-  return null
-}
-
-export default About
